81c437471d4808fe72b7e70c8073ad18076bca66
Hazel Anne Brosas <hazel_brosas@dlsu.edu.ph>
2016-11-14 15:05:55 +0800

    App

---
+++ App (Dev)/App/App/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using App.Models;
+
+namespace App.Controllers
+{
+    public class AccountController : Controller
+    {
+        AccountManager manager = new AccountManager();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult login()
+        {
+            //Account account = manager.getAccount();
+            return View();
+        }
+        
+        public ActionResult error()
+        {
+            return View();
+        }
+    }
+}
