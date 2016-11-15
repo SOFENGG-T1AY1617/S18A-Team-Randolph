ea7f4202975e0170045814bf89c92b3e8d775229
Hazel Anne Brosas <hazel_brosas@dlsu.edu.ph>
2016-11-15 20:01:12 +0800

    Cleaned everything :) As per mica's request.

--- App (Dev)/App/App/Controllers/AccountController.cs
+++ App (Dev)/App/App/Controllers/AccountController.cs
@@ -13,18 +13,43 @@ namespace App.Controllers
 
         public ActionResult Index()
         {
+            // must go to home index
             return View();
         }
 
         public ActionResult login()
         {
-            //Account account = manager.getAccount();
             return View();
         }
-        
+
+        public ActionResult register()
+        {
+            return View();
+        }
+
+        public ActionResult regerror()
+        {
+            string message = "";
+            // put code for validation (like, wrong email address, etc)
+
+
+            ViewBag.message = message;
+            return View();
+        }
+
         public ActionResult error()
         {
             return View();
         }
+
+        [HttpPost]
+        public void verify(string email, string password)
+        {
+            Console.WriteLine(email, password);
+            ViewBag.Striing = "email: " + email + " password: " + password;
+            //RedirectToRoute("Transaction/Step1");
+        }
+        
+        
     }
 }
